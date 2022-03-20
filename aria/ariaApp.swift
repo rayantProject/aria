0ac5b6a9955b5059c8f253abc1697945275b3b4d
TRAORE Rayan <rayan.traore03@gmail.com>
2022-03-20 13:43:31 +0100

    Initial Commit

---
+++ aria/ariaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ariaApp.swift
+//  aria
+//
+//  Created by apprenant72 on 20/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ariaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
